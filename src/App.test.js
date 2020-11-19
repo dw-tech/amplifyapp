7b75f85bf21299fe7f9b7f9e14c2a68fd29de08c
Dean West <dean.west@provenmethod.com>
2020-11-19 11:06:23 -0500

    Updating test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// This doesn't even run, so who cares?
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);

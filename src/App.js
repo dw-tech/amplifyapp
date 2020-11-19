2b49c2d5cc21310aeb74bce5ca2a543a45c572c8
Dean West <dean.west@provenmethod.com>
2020-11-19 11:26:48 -0500

    Adding authentication

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
 
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Some React App</h1>
+        <h1>"Secure" React App</h1>
         <p>Nobody gives a shit, dude.</p>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);

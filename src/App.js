c7a36b94da03b9581b8599e2cb353004b3490662
Dean West <dean.west@provenmethod.com>
2020-11-19 10:07:43 -0500

    Adding word 'React' to title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Some App</h1>
+        <h1>Some React App</h1>
         <p>Nobody gives a shit, dude.</p>
       </header>
     </div>
